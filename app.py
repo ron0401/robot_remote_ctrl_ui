aca039557b321e1b4f4794ecec3194a2855547e2
ron0401 <51016256+ron0401@users.noreply.github.com>
2022-06-26 22:23:23 +0900

    update

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 # coding: utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
+from copy import copy
+
 app = Flask(__name__)
 
-@app.route("/index") 
+j = open('param.json','r')
+param = json.load(j)
+@app.route("/", methods=["GET"]) 
 def index():
-   return render_template('index.html',myip = '192.168.0.196')
+    pr = copy(param)
+    req = request.args
+    pr['IMAGE_SERVER']['HOST'] = pr['IMAGE_SERVER']['HOST'] if req.get("image_server_host") == None else req.get("image_server_host")
+    pr['IMAGE_SERVER']['PORT'] = pr['IMAGE_SERVER']['PORT'] if req.get("image_server_port") == None else req.get("image_server_port")
+    pr['ROSBRIDGE_SEVER']['HOST'] = pr['ROSBRIDGE_SEVER']['HOST'] if req.get("rosbidge_server_host") == None else req.get("rosbidge_server_host")
+    pr['ROSBRIDGE_SEVER']['PORT'] = pr['ROSBRIDGE_SEVER']['PORT'] if req.get("rosbidge_server_port") == None else req.get("rosbidge_server_port")
+    return render_template('index.html',IMAGE_SERVER = pr['IMAGE_SERVER'],ROSBRIDGE_SEVER = pr['ROSBRIDGE_SEVER'])
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
